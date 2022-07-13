83f51b9dd73e9cacc49ad7719a0545cf7796983e
falbocodes <antonella.f@gmail.com>
2022-07-14 00:27:43 +0200

    chore: adds some basic k8 infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,22 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.12.1"
+      
+    }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-1"
+}
+
+# Configure the K8 Provider
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
 }
